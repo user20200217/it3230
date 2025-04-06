f470be148ccec8c54280855521ca41888c6e3ed0
user20200217 <61168693+user20200217@users.noreply.github.com>
2025-04-06 17:58:29 +0800

    Add files via upload

--- results.php
+++ results.php
@@ -1,5 +1,17 @@
+<!--
+IT 3230
+Kai Chi
+Apr 6, 2025
+-->
+
+<!DOCTYPE html>
 <html lang="en">
-    <link rel="stylesheet" href="main.css">
+    <head>
+        <meta charset="utf-8">
+        <title>Results</title>
+        <link rel="stylesheet" href="main.css">
+        <link rel="icon" href="logo.png">
+    </head>
     <body>
         <div id="wrapper">
             <div id="header">
@@ -14,15 +26,30 @@
                 </ul>
             </div>
 
-            <div id="main_grid3">
+            <div id="main_grid4">
 <?php
     if (isset($_POST['year']) && isset($_POST['make']) && isset($_POST['model'])) {
         print("<h2>You searched: ".$_POST['year']." ".$_POST['make']." ".$_POST['model']."</h2>");
         if ($_POST['year']==2010 && strtolower($_POST['make'])=='chevy' && strtolower($_POST['model'])=='hhr') {
             print("<style>\n");
-            print("#main_grid3 {\n");
-            print("grid-template-areas: \"h2 h2 h2 h2\"");
-            print("\"item1       item2       item3       item4\";");
+            print("#main_grid4 {\n");
+            print("grid-template-areas: \"h2 h2 h2 h2\"\n");
+            print("\"item1       item2       item3       item4\"\n;");
+            print("}\n");
+            print("</style>\n");
+            print("<div id=\"item1\"><div></div><a href=\"details.html\">2010 Chevy HHR<br>$1500</a></div>\n");
+            print("<div id=\"item2\"><div></div><a href=\"details.html\">2010 Chevy HHR<br>$1500</a></div>\n");
+            print("<div id=\"item3\"><div></div><a href=\"details.html\">2010 Chevy HHR<br>$1500</a></div>\n");
+            print("<div id=\"item4\"><div></div><a href=\"details.html\">2010 Chevy HHR<br>$1500</a></div>\n");
+        }
+    } 
+    elseif (isset($_POST['min_price']) && isset($_POST['max_price'])) {
+        print("<h2>You searched: between $".$_POST['min_price']." and $".$_POST['max_price']."</h2>");
+        if ($_POST['min_price']<=1500 && $_POST['max_price']>=1500) {
+            print("<style>\n");
+            print("#main_grid4 {\n");
+            print("grid-template-areas: \"h2 h2 h2 h2\"\n");
+            print("\"item1       item2       item3       item4\"\n;");
             print("}\n");
             print("</style>\n");
             print("<div id=\"item1\"><div></div><a href=\"details.html\">2010 Chevy HHR<br>$1500</a></div>\n");
